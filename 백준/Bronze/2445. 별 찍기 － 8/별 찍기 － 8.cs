bcf88d42e62db29e71ab8899d4bd38f48be5f471
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 08:41:48 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 1684 ms, Memory: 6588 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int star1 = 1; star1 <=i; star1++)
+                {
+                    Console.Write("*");
+                }
+                for (int space = 1; space <= n*2 -(i*2); space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int star2 = 1; star2 <=i; star2++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 1; i <= n-1; i++)
+            {
+                for (int star1 = i; star1 <= n-1; star1++)
+                {
+                    Console.Write("*");
+                }
+                for (int space = 1; space <= 2*i; space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int star1 = i; star1 <= n - 1; star1++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
