aebb78127ebe3514175a27df2941ca02e15907ba
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 08:50:34 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 64 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace baekjun
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input.Length);
+        }
+
+    }
+}
