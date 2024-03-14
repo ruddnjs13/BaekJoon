13ec1cead73efd571d2dfb803153d0384dbb693f
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 08:55:34 +0900

    [Bronze IV] Title: 줄번호, Time: 64 ms, Memory: 5808 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine($"{i}. {input}");
+            }
+        }
+    }
+}
