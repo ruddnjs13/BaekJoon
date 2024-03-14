41ac5712c5c66c35d23ff4e36bcba980893ac9c6
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 08:44:44 +0900

    [Bronze V] Title: Hello World, Time: 60 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace test
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
