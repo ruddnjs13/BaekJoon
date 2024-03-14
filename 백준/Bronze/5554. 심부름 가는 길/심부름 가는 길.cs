9c71db84c775dd19d5e12a17ba042a153f2430ce
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 08:56:46 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((a + b + c + d) / 60);
+            Console.WriteLine((a + b + c + d) % 60);
+        }
+    }
+}
