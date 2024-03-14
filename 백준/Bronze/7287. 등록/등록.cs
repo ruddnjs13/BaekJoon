f8acb544e69445d50aa44782a8428345f1457ac0
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 08:58:36 +0900

    [Bronze V] Title: 등록, Time: 60 ms, Memory: 5380 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(115);
+            Console.WriteLine("ruddnjs13");
+        }
+    }
+}
