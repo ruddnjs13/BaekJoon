100b288f350586de30d95997a78ee52c98642c96
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 10:08:30 +0900

    [Bronze V] Title: 직사각형, Time: 60 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*b);
+        }
+    }
+}
