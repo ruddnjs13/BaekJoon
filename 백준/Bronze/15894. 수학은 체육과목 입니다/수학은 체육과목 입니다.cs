5b4c4075424a105f75ea992ff86bb9bcc80e88e9
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 09:11:49 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 60 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine(n * 3 + n);
+        }
+    }
+}
