f5d878d65d9e07409260294661d218fe93117abc
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 09:10:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 60 ms, Memory: 5440 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace test
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int ascii = Convert.ToInt32(Console.Read());
+            Console.Write("{0}",ascii);
+        }
+    }
+}
