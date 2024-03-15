fab476a8cbdcf153f99a22f8556f1660ab384775
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 10:08:48 +0900

    [Bronze V] Title: 문자와 문자열, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cs
@@ -0,0 +1,13 @@
+namespace baekjun
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine(input[i - 1]);
+        }
+
+    }
+}
