e14a9b46c5088e75298548d41d9c582a2e950055
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 09:01:06 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace baekjun
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( \')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+
+    }
+}
