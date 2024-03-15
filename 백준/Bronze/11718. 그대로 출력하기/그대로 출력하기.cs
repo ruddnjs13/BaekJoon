6353a4a2bff4c4d32279a7d9939ab4fcb5f6835f
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 09:10:37 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 68 ms, Memory: 5384 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
