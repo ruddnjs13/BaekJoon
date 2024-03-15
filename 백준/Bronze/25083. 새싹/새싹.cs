157cb9bfc2ecc10e5a8c862de153f4c543855e60
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 09:13:00 +0900

    [Bronze V] Title: 새싹, Time: 52 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Test123
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("         ,r\'\"7");
+            Console.WriteLine("r`-_   ,\'  ,/");
+            Console.WriteLine(" \\. \". L_r\'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}
