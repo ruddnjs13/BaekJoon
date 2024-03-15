a1683163c28f4eebf5c30b48aaaa1fd166f4eb46
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-15 09:02:20 +0900

    [Bronze V] Title: 오늘 날짜, Time: 60 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.UtcNow.ToString("yyyy-MM-dd"));
+        }
+    }
+}
