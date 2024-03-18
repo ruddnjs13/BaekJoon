f7e5a6c964b9a03d12d3f7855604dbd1e3e661d4
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-03-18 22:34:19 +0900

    [Bronze V] Title: 웰컴, Time: 56 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".  .   .");
+            Console.WriteLine("|  | _ | _. _ ._ _  _");
+            Console.WriteLine("|/\\|(/.|(_.(_)[ | )(/.");
+        }
+    }
+}
