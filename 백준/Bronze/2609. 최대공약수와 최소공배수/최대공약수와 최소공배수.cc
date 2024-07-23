5abbfa4ccd7655f433774fe9fc02b0349bc8ff43
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-07-23 21:25:07 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+	int c = a % b;
+	while (c != 0) {
+		a = b;
+		b = c;
+		c = a % b;
+	}
+	return b;
+}
+
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << gcd(a, b) << "\n" << lcm(a, b);
+}
