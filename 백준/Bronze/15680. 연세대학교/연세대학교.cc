3de4fdf1c6e5f573a753f19b05a4a4ea45317b8b
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-07-26 23:04:14 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+	int n;
+	cin >> n;
+	if (n == 0) {
+		cout << "YONSEI";
+	}
+	else if (n == 1) {
+		cout << "Leading the Way to the Future";
+	}
+}
