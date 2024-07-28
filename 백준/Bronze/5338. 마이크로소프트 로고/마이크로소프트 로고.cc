8a549a30622adfa6b072ed8715e1d5c3b09a8bbb
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-07-28 15:18:15 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str = R"(       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|)";
+	cout << str;
+	return 0;
+}
