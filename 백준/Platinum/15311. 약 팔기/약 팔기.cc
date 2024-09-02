36e5fd1b3d770611a4312bed67c64bd5161246ad
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-09-03 00:37:52 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << 2000 << '\n';
+
+	for (int i = 0; i < 1000; i++) {
+		cout << 1 << ' ';
+	}
+	for (int i = 0; i < 1000; i++) {
+		cout << 1000 << ' ';
+	}
+}
