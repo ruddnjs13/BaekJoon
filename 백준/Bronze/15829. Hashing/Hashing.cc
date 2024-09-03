b6ebfd8271d90ed162b81f4a9d9ffe493a7dba31
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-09-03 20:04:17 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -6,14 +6,17 @@ using namespace std;
 
 int main()
 {
-	int l, answer = 0;
+	int l;
+	unsigned long long answer = 0, lkw = 1;
 	cin >> l;
 
 	string input;
 	cin >> input;
-	for (int i = 0; i < l; i++) {
-		answer +=  (input[i] - 96)  * pow(31,i);
+	for (int i = 0; i < l; i++) { 
+		answer += ((input[i] - 96) * lkw);
+
+		lkw = lkw * 31 % 1234567891;
 	}
 
-	cout << answer;
+	cout << answer % 1234567891;
 }
