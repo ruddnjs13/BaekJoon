3279f157c54967a96894a60717b6a2bce5d6fc4b
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-09-21 11:33:23 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 84 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<string>
+#include<stack>
+
+using namespace std;
+
+
+int main()
+{
+	string input;
+
+	while (true)
+	{
+		getline(cin, input);
+		if (input == ".") {
+			break;
+		}
+		stack<char> checkList;
+		bool flag = false;
+
+		for (int i = 0; i < input.size(); i++) {
+			if (input[i] == '(' || input[i] == '[') {
+				checkList.push(input[i]);
+			}
+			if (input[i] == ')') {
+				if (input[i] == ')') {
+					if (!checkList.empty() && checkList.top() == '(') {
+						checkList.pop();
+					}
+					else {
+						flag = true;
+						break;
+					}
+				}
+			}
+			if (input[i] == ']') {
+				if (input[i] == ']') {
+					if (!checkList.empty() && checkList.top() == '[') {
+						checkList.pop();
+					}
+					else {
+						flag = true;
+						break;
+					}
+				}
+			}
+		}
+		if (!flag && checkList.empty()) {
+			cout << "yes" << '\n';
+		}
+		else {
+			cout << "no" << '\n';
+		}
+	}
+}
