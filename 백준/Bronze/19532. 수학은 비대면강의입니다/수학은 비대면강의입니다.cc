fa94e886f7a874b93efb8c7a357110e272300205
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2024-11-11 23:18:08 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	bool flag = false;
+
+	for (int i = -999; i <= 999; i++) {
+		for (int j = -999; j <= 999; j++) {
+			if (((a * i + b * j) == c) && ((d * i + e * j) == f)) {
+				cout << i << ' ' << j;
+				flag = true;
+				break;
+			}
+		}
+		if (flag) {
+			break;
+		}
+	}
+}
