a8f53b7dd51fd8125a349bf727d554e42da4d771
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2025-01-01 23:27:22 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, k, a, b;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+	}
+
+	cout << "비와이";
+}
