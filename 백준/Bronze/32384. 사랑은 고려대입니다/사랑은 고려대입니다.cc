47f2839ee412c7976e6194a34b6727c6ccc1bd16
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2025-01-03 22:37:16 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cout << "LoveisKoreaUniversity ";
+	}
+}
