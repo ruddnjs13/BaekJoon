5ff0b72b6626d6eda123bc2cf9ea1c47334bec6d
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2025-01-14 23:33:54 +0900

    [Bronze V] Title: A+B - 9, Time: 60 ms, Memory: 5952 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Specialized;
+using System.Numerics;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger[] input;
+            input = Array.ConvertAll(Console.ReadLine().Split(), BigInteger.Parse);
+            Console.WriteLine(input[0] + input[1]);
+        }
+    }
+}
