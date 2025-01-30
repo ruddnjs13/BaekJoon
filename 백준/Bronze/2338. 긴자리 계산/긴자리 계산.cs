f460535603941317daa4a78c513622ac3dae0dd0
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2025-01-30 22:15:40 +0900

    [Bronze V] Title: 긴자리 계산, Time: 52 ms, Memory: 5704 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Specialized;
+using System.Numerics;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger a, b;
+            a = BigInteger.Parse(Console.ReadLine());
+            b = BigInteger.Parse(Console.ReadLine());
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+        }
+    }
+}
