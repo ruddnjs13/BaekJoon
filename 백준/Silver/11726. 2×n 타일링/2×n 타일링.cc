8084730bfbc4862ff00806e5879a0846e1223236
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2025-03-09 22:37:33 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long dp[1001]{ 0, };
+    int n;
+    cin >> n;
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = ((dp[i - 1] + dp[i - 2]) % 10007);
+    }
+    cout << dp[n];
+}
