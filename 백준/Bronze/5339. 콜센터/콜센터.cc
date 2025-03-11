63d910a4965bf0f766552db02f4134998346bd7f
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2025-03-11 23:35:39 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s = R"(     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |)";
+	cout << s;
+}
