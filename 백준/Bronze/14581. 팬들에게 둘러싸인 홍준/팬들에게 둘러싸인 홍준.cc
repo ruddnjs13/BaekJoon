d91c9333846c3e9c124e5909968adecf83194406
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2025-03-20 22:20:28 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string input;
+    cin >> input;
+
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << input << "::fan:\n";
+    cout << ":fan::fan::fan:";
+}
