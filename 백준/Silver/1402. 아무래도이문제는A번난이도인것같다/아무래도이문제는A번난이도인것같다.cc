e136812ae143de1fff1438d1061f6aa8f3ccca96
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2025-04-25 08:56:33 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+
+    int a, b;
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        cin >> a >> b;
+        cout << "yes" << '\n';
+    }
+}
