166572ad53847a70777e4ae9322561223eeb1acb
ruddnjs13 <157551045+ruddnjs13@users.noreply.github.com>
2025-07-28 21:48:54 +0900

    [Gold III] Title: 줄 세우기, Time: 76 ms, Memory: 3832 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n, m, a, b;
+
+	cin >> n >> m;
+
+	vector<vector<int>> graph(n + 1);
+	vector<int> v = vector<int>(n+1, 0);
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> a >> b;
+
+		graph[a].push_back(b);
+		v[b]++;
+	}
+
+	queue<int> q;
+
+	for (int i = 1; i < v.size(); i++)
+	{
+		if (v[i] == 0)
+			q.push(i);
+	}
+
+	while (q.empty() == false)
+	{
+		int node = q.front();
+		q.pop();
+
+		cout << node << ' ';
+
+		for (auto i : graph[node])
+		{
+			v[i]--;
+			if (v[i] == 0)
+				q.push(i);
+		}
+	}
+}
